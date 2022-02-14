c631ed643a4591a213d64658f5cd6ca0c9e12d61
2k16daniel <2k16daniel@gmail.com>
2022-02-14 15:18:26 +0800

    added item_id for every collection

--- EWater_api/models.py
+++ EWater_api/models.py
@@ -7,6 +7,7 @@ class ClientData(models.Model):
     email = models.CharField(max_length=30,blank=False, default='')
 
 class OrderData(models.Model):
+    item_id = models.PositiveIntegerField(blank=True, null=True)
     phone = models.CharField(max_length=11, blank=False, default='')
     fullname = models.CharField(max_length=50,blank=False, default='')
     address = models.CharField(max_length=50,blank=False, default='')
@@ -17,6 +18,7 @@ class OrderData(models.Model):
     reservationDate = models.DateTimeField()
 
 class OrderStatus(models.Model):
+    item_id = models.PositiveIntegerField(blank=True, null=True)
     phone = models.CharField(max_length=11, blank=False, default='')
     fullname = models.CharField(max_length=50,blank=False, default='')
     address = models.CharField(max_length=50,blank=False, default='')
@@ -28,6 +30,8 @@ class OrderStatus(models.Model):
     DateAccepted = models.DateTimeField(auto_now_add=True)
 
 class Order_stats_sales(models.Model):
+    item_id = models.PositiveIntegerField(blank=True, null=True)
     item = models.CharField(max_length=15,blank=False, default='GALLON 1')
+    quantity = models.IntegerField(blank=True, null=True)
     price = models.IntegerField(blank=True, null=True)
     date = models.DateTimeField(auto_now_add=True)
